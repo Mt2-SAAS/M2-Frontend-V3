3cfdc3273f8cd772111262aa811606d471262714
luisito666 <luispenagos91@gmail.com>
2022-12-12 21:46:58 -0500

    Add parameter to initialData

--- src/app/store/interfaces/initial_data.interfaces.ts
+++ src/app/store/interfaces/initial_data.interfaces.ts
@@ -1,5 +1,6 @@
 
 export interface InitialData {
+    id : string;
     name : string;
     slug : string;
     images : Image[];
--- src/app/store/reducers/initial_data.reducers.ts
+++ src/app/store/reducers/initial_data.reducers.ts
@@ -16,6 +16,7 @@ export interface InitialDataState {
 
 export const InititalDataInitialState: InitialDataState = {
     data: {
+        id: '',
         name : '',
         slug : '',
         images : [],
